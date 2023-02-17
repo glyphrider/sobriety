ddff9ba9bd2bf458c20fb116023ff1c659f690bc
Brian H. Ward <glyphrider@gmail.com>
2023-02-17 11:47:27 -0500

    component architecture
    moved App to lib.rs
    created components for Coin and Continuous Sobriety
    passing values as Props into components

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,5 @@
-use yew::prelude::*;
-use chrono::{ Local, NaiveDate, NaiveDateTime };
+use sobriety::App;
 
-fn get_now() -> NaiveDateTime {
-    Local::now().naive_local()
-}
-
-#[function_component]
-fn App() -> Html {
-    let sober = NaiveDate::from_ymd_opt(2009,6,20).unwrap();
-    let now = use_state(|| get_now());
-    let formatted_date = sober.format("%A, %B %e, %Y");
-    let years = now.date().years_since(sober).unwrap();
-    let roman = roman::convert::to(years as u16);
-    let days = now.date().signed_duration_since(sober).num_days();
-
-    let cloned_now = now.clone();
-    gloo::timers::callback::Interval::new(5000, move || {
-        cloned_now.set(get_now());
-    }).forget();
-
-    html! {
-        <div class="sobriety">
-            <h1>{ "My sobriety timeline" }</h1>
-            <p>{ formatted_date }<i>{ " through today" }</i></p>
-            <div class="years">
-                { roman }
-            </div>
-            <div class="days">
-                { days } { " days of continuous sobriety" }
-            </div>
-        </div>
-    }
-}
-
-fn main() {
+pub fn main() {
     yew::Renderer::<App>::new().render();
 }
